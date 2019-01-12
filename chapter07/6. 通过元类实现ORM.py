3cf53ebc5f4699b31d441e7cb18ac56509424670
Administrator <240942649@qq.com>
2019-01-12 18:18:54 +0800

    update

--- chapter07/6. 通过元类实现ORM.py
+++ chapter07/6. 通过元类实现ORM.py
@@ -2,13 +2,17 @@
 # 需求
 
 
-class CharFiled(object):
+class Filed(object):
+    pass
+
+
+class CharFiled(Filed):
     def __init__(self, db_column, max_length=None):
         self._value = None
         if max_length is None:
             raise ValueError('max_length 是必须的')
 
-        elif isinstance(max_length, int):
+        elif not isinstance(max_length, int):
             raise ValueError('max_length 必须是int')
 
         elif max_length <= 0:
@@ -31,7 +35,7 @@ class CharFiled(object):
         pass
 
 
-class IntFiled(object):
+class IntFiled(Filed):
 
     # 数据描述符
     def __init__(self, db_column, min_value=None, max_value=None):
@@ -69,11 +73,60 @@ class IntFiled(object):
 
 
 class ModelMetaClass(type):
-    def __new__(cls, *args, **kwargs):
-        pass
+    def __new__(cls, name, bases, attrs, **kwargs):
+
+        if name == 'BaseModel':
+            return super().__new__(cls, name, bases, attrs, **kwargs)
+
+        fields = dict()
+        for key, value in attrs.items():
+            if isinstance(value, Filed):
+                fields[key] = value
+
+        _meta = {}
+        attrs_meta = attrs.get('Meta', None)
+        db_table = name.lower()
+        if attrs_meta is not None:
+            table = getattr(attrs_meta, 'db_table', None)
+            if table is not None:
+                db_table = table
+            del attrs['Meta']
+            del attrs_meta
+
+        _meta['db_table'] = db_table
+        attrs['_meta'] = _meta
+        attrs['fields'] = fields
+
+        return super().__new__(cls, name, bases, attrs, **kwargs)
 
 
-class User(object):
+class BaseModel(metaclass=ModelMetaClass):
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        super().__init__(*args, **kwargs)
+
+    def save(self):
+        fields = []
+        values = []
+        for key, value in self.fields.items():
+            db_column = value.db_column
+            if db_column == '':
+                db_column = key.lower()
+                fields.append(db_column)
+            db_value = getattr(self, key)
+            if isinstance(db_value, str):
+                db_value = ''.join(["'", db_value, "'"])
+            values.append(str(db_value))
+
+        sql = 'insert {db_table}({fields}) values({values})'.format(db_table=self._meta['db_table'],
+                                                                    fields=','.join(fields),
+                                                                    values=','.join(values))
+        print(sql)
+
+
+class User(BaseModel):
+
     name = CharFiled(db_column='', max_length=10)
     age = IntFiled(db_column='', min_value=0, max_value=120)
 
